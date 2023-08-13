14131c6fcaaa6317bcc8225d7e5496f161a1549c
Khizar Amin <kamin@bridgelinxpk.com>
2023-08-13 14:52:54 +0500

    added medium to navbar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import LinkedInIcon from "@material-ui/icons/LinkedIn"
 import GitHubIcon from "@material-ui/icons/GitHub"
 import EmailIcon from "@material-ui/icons/Email"
+import MediumIcon from "../assets/medium-inverted.png"
 
 import '../styles/Footer.css'
 
@@ -9,6 +10,7 @@ function Footer() {
   return (
     <div className='footer'>
         <div className="socialMedia">
+          <a href='https://medium.com/@khizaramin95'><img src={MediumIcon} style={{width:"4pc", margin:"20px"}} /></a>
           <a href='https://www.linkedin.com/in/k-amin07'><LinkedInIcon /></a>
           <a href='mailto:khizaramin95@gmail.com'><EmailIcon /></a>
           <a href='https://www.github.com/k-amin07'><GitHubIcon /></a>
