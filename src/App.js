c556607811b4e17017ed00f7ca230bb77940db95
Khizar Amin <kamin@bridgelinxpk.com>
2023-08-14 01:04:56 +0500

    updated routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Experience from './pages/Experience';
@@ -14,10 +14,11 @@ function App() {
       <Router>
         <Navbar/>
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/projects" element={<Projects/>} />
-          <Route path="/project/:id" element={<ProjectDisplay/>} />
-          <Route path="/experience" element={<Experience />} />
+          <Route path="/" element={<Navigate to="/portfolio" replace/>}/>
+          <Route path="/portfolio" element={<Home/>} />
+          <Route path="/portfolio/projects" element={<Projects/>} />
+          <Route path="/portfolio/project/:id" element={<ProjectDisplay/>} />
+          <Route path="/portfolio/experience" element={<Experience />} />
         </Routes>
         <Footer/>
       </Router>
